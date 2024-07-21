70ce86a9f8326166db46eae6086a5bbf7a959a6a
Adam Huganir <adam@huganir.com>
2024-07-21 17:56:07 -0400

    rgb keys are now working, but no underglow yet

--- kb.py
+++ kb.py
@@ -1,14 +1,42 @@
+from time import sleep
+
 import board
 
-from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
+from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard, debug_error
 from kmk.scanners import DiodeOrientation
+from kmk.extensions.peg_rgb_matrix import Rgb_matrix as _Rgb_matrix, Rgb_matrix_data as _Rgb_matrix_data
 
 
 class KMKKeyboard(_KMKKeyboard):
     row_pins = [getattr(board, f"GP{row}") for row in (8, 6, 19, 20, 18)]
     col_pins = [getattr(board, f"GP{col}") for col in (26, 27, 4, 5, 1, 23, 22, 21, 28, 3, 7, 12, 13, 14, 15)]
     diode_orientation = DiodeOrientation.ROW2COL
+    rgb_pixel_pin = board.GP2
+    num_pixels = 89
+    disable_auto_write = False
+    brightness_limit = 1.0
+    led_key_pos = list(range(89))
 
-    def __init__(self, *args,  **kwargs):
-        print(self.keymap)
-        super().__init__(*args, **kwargs)
+    def before_matrix_scan(self) -> None:
+        for module in self.modules:
+            try:
+                module.before_matrix_scan(self)
+            except Exception as err:
+                debug_error(module, "before_matrix_scan", err)
+
+        for ext in self.extensions:
+            try:
+                ext.before_matrix_scan(self.sandbox)
+            except Exception as err:
+                debug_error(ext, "before_matrix_scan", err)
+
+class Rgb_matrix(_Rgb_matrix):
+    def increase_brightness(self, step=None):
+
+        for i, val in enumerate(self.ledDisplay):
+            self.neopixel[self.keyPos[i]] = (0,0,0)
+        for i, val in enumerate(self.ledDisplay):
+            if i > 75:
+                print(i)
+                self.neopixel[self.keyPos[i]] = (val[0], val[1], val[2])
+                sleep(1)
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from kb import KMKKeyboard
+from kb import KMKKeyboard, Rgb_matrix
 from kmk.keys import KC
 from kmk.modules.layers import Layers
+from kmk.extensions.peg_rgb_matrix import Color
 
 keyboard = KMKKeyboard()
 layers = Layers()
+rgb = Rgb_matrix(ledDisplay=75 * [Color.WHITE] + 14 * [Color.OFF])
 keyboard.modules.append(layers)
+keyboard.extensions.append(rgb)
 
 # Key aliases
-______ = KC.TRNS
-XXXXXX = KC.NO
+____ = KC.TRNS
+XXXX = KC.NO
 
 LOWER = KC.MO(1)
 RAISE = KC.MO(2)
@@ -16,19 +19,26 @@ RAISE = KC.MO(2)
 # fmt:off
 keymaps = [
     [
-        "ESC",       "N1",      "N2",      "N3",     "N4",     "N5",     "N6",     "N7",     "N8",      "N9",      "N0",       "MIN",      "EQ",       "BKSP",    "DEL",
-        "TAB",       "Q",       "W",       "E",      "R",      "T",      "Y",      "U",      "I",       "O",       "P",        "LBRKT",    "RBRKT",    "BKSP",    "DEL",
-        "CAPS",      "A",       "S",       "D",      "F",      "G",      "H",      "J",      "K",       "L",       "SCLN",     "QUOT",     "ENT",      "ENT",     "PGUP",
-        "LSHIFT",    "Z",       "X",       "C",      "V",      "B",      "N",      "M",      "COMM",    "DOT",     "SLSH",     "RSFT",     "RSFT",     "UP",      "PGDN",
-        "LCTL",      "LGUI",    "LALT",    "SPC",    "SPC",    "SPC",    "SPC",    "SPC",    "RALT",    "RGUI",    "RCTL",     "MO",       "LEFT",     "DOWN",    "RGHT",
+        "ESC",     "N1",    "N2",    "N3",   "N4",   "N5",   "N6",   "N7",   "N8",    "N9",    "N0",    "MINS",  "EQL",   "BSLS", "GRV",
+        "TAB",     "Q",     "W",     "E",    "R",    "T",    "Y",    "U",    "I",     "O",     "P",     "LBRC",  "RBRC",  "BSPC", "DEL",
+        "CAPS",    "A",     "S",     "D",    "F",    "G",    "H",    "J",    "K",     "L",     "SCLN",  "QUOT",  "ENT",   "ENT",  "PGUP",
+        "LSHIFT",  "Z",     "X",     "C",    "V",    "B",    "N",    "M",    "COMM",  "DOT",   "SLSH",  "RSFT",  LOWER,   "UP",   "PGDN",
+        "LCTL",    "LGUI",  "LALT",  "SPC",  "SPC",  "SPC",  "SPC",  "SPC",  "SPC",  "RALT",   "RCTL",  "RCTL",  "LEFT",  "DOWN", "RGHT",
+   ],
+        [
+        "RGB_BRI", ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,
+        "RGB_BRD", ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,
+        "RGB_TOG", ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,
+        ____,      ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,
+        ____,      ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,
+   ],
 
-    ],
 ]
 # fmt:on
 
 for layer in keymaps:
     for idx, key in enumerate(layer):
-        layer[idx] = KC[key]
+        layer[idx] = KC[key] if isinstance(key, str) else key
 keyboard.keymap = keymaps
 
 if __name__ == "__main__":
