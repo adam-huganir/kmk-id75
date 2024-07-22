0db40a4fdfa0e6002e576cc7414702c5aec60d8c
Adam Huganir <adam@huganir.com>
2024-07-22 10:06:08 -0400

    undo all troublehsooting changes
    (cherry picked from commit d8f96e9c16600bf676f8ea9b892a7e080ed9b8ba)

--- kb.py
+++ kb.py
@@ -1,46 +1,27 @@
 import board
-import neopixel
 
 from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
 from kmk.scanners import DiodeOrientation
-from kmk.extensions.peg_rgb_matrix import Rgb_matrix as _Rgb_matrix
 
 
 class KMKKeyboard(_KMKKeyboard):
     row_pins = [getattr(board, f"GP{row}") for row in (8, 6, 19, 20, 18)]
     col_pins = [getattr(board, f"GP{col}") for col in (26, 27, 4, 5, 1, 23, 22, 21, 28, 3, 7, 12, 13, 14, 15)]
     diode_orientation = DiodeOrientation.ROW2COL
+    rgb_pixel_pin = board.GP2
+    num_pixels = 75
+    disable_auto_write = False
+    brightness_limit = 0.5
+    brightness_step=0.025
+    led_key_pos = list(range(75))
 
-
-class Rgb_matrix(_Rgb_matrix):
-    def __init__(
-        self,
-        rgb_order=(1, 0, 2),
-        disable_auto_write=False,
-        led_display=None,
-        rgb_pixel_pin=board.GP2,
-        num_pixels=75,
-        brightness_limit=0.1,
-        brightness_step=0.025,
-        led_key_pos=None,
-    ):
-        if led_display is None:
-            led_display = []
-        super().__init__(rgb_order=rgb_order, disable_auto_write=disable_auto_write, ledDisplay=led_display)
-        self.pin = rgb_pixel_pin
-        self.num_pixels = num_pixels
-        self.brightness_limit = brightness_limit
-        self.led_key_pos = self.keyPos = led_key_pos or list(range(75))
-        self.brightness_step = brightness_step
-        self.neopixel = None
-
-    def during_bootup(self, board):
-        self.neopixel = neopixel.NeoPixel(
-            self.pin,
-            self.num_pixels,
-            brightness=self.brightness_limit,
-            pixel_order=self.rgb_order,
-            auto_write=not self.disable_auto_write,
+    def __init__(self):
+        # create and register the scanner
+        self.matrix = MatrixScanner(
+            self.col_pins,
+            self.row_pins,
+            diode_orientation=self.diode_orientation,
+            pull=digitalio.Pull.DOWN,
+            rollover_cols_every_rows=None,
+            offset=0,
         )
-        self.on()
-        return
--- main.py
+++ main.py
@@ -1,32 +1,17 @@
-import board
-
-from kmk.keys import KC
+from kb import KMKKeyboard
 from kmk.modules.layers import Layers
-from kmk.scanners import DiodeOrientation
-from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
-
-
-class KMKKeyboard(_KMKKeyboard):
-    row_pins = [getattr(board, f"GP{row}") for row in (8, 6, 19, 20, 18)]
-    col_pins = [getattr(board, f"GP{col}") for col in (26, 27, 4, 5, 1, 23, 22, 21, 28, 3, 7, 12, 13, 14, 15)]
-    diode_orientation = DiodeOrientation.ROW2COL
-
+from kmk.extensions.peg_rgb_matrix import Color
+from kmk.extensions.peg_rgb_matrix import Rgb_matrix
 
 keyboard = KMKKeyboard()
 layers = Layers()
-keyboard.modules.extend((layers,))
+rgb = Rgb_matrix(ledDisplay=75 * [Color.WHITE])
+keyboard.modules.append(layers)
+keyboard.extensions.append(rgb)
 
-# fmt: off
-keyboard.keymap = keymaps = [
-    [
-        KC.ESC,     KC.N1,    KC.N2,    KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,    KC.N9,    KC.N0,    KC.MINS,  KC.EQL,   KC.BSLS, KC.GRV,
-        KC.TAB,     KC.Q,     KC.W,     KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,     KC.O,     KC.P,     KC.LBRC,  KC.RBRC,  KC.BSPC, KC.DEL,
-        KC.CAPS,    KC.A,     KC.S,     KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,     KC.L,     KC.SCLN,  KC.QUOT,  KC.ENT,   KC.ENT,  KC.PGUP,
-        KC.LSHIFT,  KC.Z,     KC.X,     KC.C,    KC.V,    KC.B,    KC.N,    KC.M,    KC.COMM,  KC.DOT,   KC.SLSH,  KC.RSFT,  KC.RSFT,  KC.UP,   KC.PGDN,
-        KC.LCTL,    KC.LGUI,  KC.LALT,  KC.SPC,  KC.SPC,  KC.SPC,  KC.SPC,  KC.SPC,  KC.SPC,  KC.RALT,   KC.RCTL,  KC.RCTL,  KC.LEFT,  KC.DOWN, KC.RGHT,
-   ],
-]
-# fmt: on
+from keymaps import default
+
+keyboard.keymap = default
 
 if __name__ == "__main__":
     keyboard.debug_enabled = True
