4dbf59cb2fde008419e3781e8c9c8dec4717cb41
Adam Huganir <adam@huganir.com>
2024-07-24 22:20:58 -0400

    simplest keyboard

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import board
 
-from kb import KMKKeyboard, # Rgb_matrix
-# from kmk.extensions.peg_rgb_matrix import Color
-# from kmk.modules.layers import Layers
+from kmk.keys import KC
+from kmk.modules.layers import Layers
+from kmk.scanners import DiodeOrientation
+from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
+
+
+class KMKKeyboard(_KMKKeyboard):
+    row_pins = [getattr(board, f"GP{row}") for row in (8, 6, 19, 20, 18)]
+    col_pins = [getattr(board, f"GP{col}") for col in (26, 27, 4, 5, 1, 23, 22, 21, 28, 3, 7, 12, 13, 14, 15)]
+    diode_orientation = DiodeOrientation.ROW2COL
+
 
 keyboard = KMKKeyboard()
-# layers = Layers()
-# rgb = Rgb_matrix(
-#     rgb_pixel_pin=board.GP2,
-#     num_pixels=75,
-#     disable_auto_write=False,
-#     brightness_limit=0.25,
-#     led_key_pos=list(range(75)),
-#     led_display=75 * [Color.PURPLE],
-# )
-# keyboard.modules.extend((layers,))
-# keyboard.extensions.extend((rgb,))
+layers = Layers()
+keyboard.modules.extend((layers,))
 
-from keymaps import default # noqa:  E402
-
-keyboard.keymap = default
+# fmt: off
+keyboard.keymap = keymaps = [
+    [
+        KC.ESC,     KC.N1,    KC.N2,    KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,    KC.N9,    KC.N0,    KC.MINS,  KC.EQL,   KC.BSLS, KC.GRV,
+        KC.TAB,     KC.Q,     KC.W,     KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,     KC.O,     KC.P,     KC.LBRC,  KC.RBRC,  KC.BSPC, KC.DEL,
+        KC.CAPS,    KC.A,     KC.S,     KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,     KC.L,     KC.SCLN,  KC.QUOT,  KC.ENT,   KC.ENT,  KC.PGUP,
+        KC.LSHIFT,  KC.Z,     KC.X,     KC.C,    KC.V,    KC.B,    KC.N,    KC.M,    KC.COMM,  KC.DOT,   KC.SLSH,  KC.RSFT,  KC.RSFT,  KC.UP,   KC.PGDN,
+        KC.LCTL,    KC.LGUI,  KC.LALT,  KC.SPC,  KC.SPC,  KC.SPC,  KC.SPC,  KC.SPC,  KC.SPC,  KC.RALT,   KC.RCTL,  KC.RCTL,  KC.LEFT,  KC.DOWN, KC.RGHT,
+   ],
+]
+# fmt: on
 
 if __name__ == "__main__":
     keyboard.debug_enabled = True
